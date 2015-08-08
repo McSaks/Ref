cc6b28687c3dae528fbe70ea559cc6b58963f047
Max D. Kheymits <mcsaksik@gmail.com>
2015-08-08 15:15:23 +0300

    Documentation pages added
    My first experience in writing documentations.
    Tutorial is in project.

---
+++ PacletInfo.m
@@ -0,0 +1,21 @@
+Paclet[
+Name -> "Ref",
+Version -> "1.0.0",
+MathematicaVersion -> "9+",
+Extensions -> {{
+    "Documentation",
+    Resources -> {
+        "Guides/RefPackage",
+        "ReferencePages/Symbols/Ref",
+        "ReferencePages/Symbols/NewRef",
+        "ReferencePages/Symbols/EvalRef",
+        "ReferencePages/Symbols/UnRef",
+        "ReferencePages/Symbols/SetRef",
+        "ReferencePages/Symbols/Refs",
+        "ReferencePages/Symbols/RefBlock",
+        "ReferencePages/Symbols/RefSetQ",
+        "ReferencePages/Symbols/RefNullQ"
+    },
+    Language -> "English"
+}}
+]
